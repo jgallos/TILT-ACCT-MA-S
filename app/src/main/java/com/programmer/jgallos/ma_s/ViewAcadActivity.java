1c91d6958c6f27797d89bad5a45cb18aca154dfd
jgallos <hsitechnologies@gmail.com>
2019-01-31 04:16:25 +0800

    31/01/2019 4:16am

--- app/src/main/java/com/programmer/jgallos/ma_s/ViewAcadActivity.java
+++ app/src/main/java/com/programmer/jgallos/ma_s/ViewAcadActivity.java
@@ -7,14 +7,56 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import com.squareup.picasso.Picasso;
+
 public class ViewAcadActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_acad);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        recyclerView = (RecyclerView)findViewById(R.id.recyclerViewAcad);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Android_Development");
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+            }
+        };
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +66,60 @@ public class ViewAcadActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+
+        FirebaseRecyclerAdapter<AcadRecords, AcadViewHolder> FBRA = new FirebaseRecyclerAdapter<AcadRecords, AcadViewHolder>(
+                AcadRecords.class,
+                R.layout.card_items,
+                AcadViewHolder.class,
+                mDatabase
+        ) {
+            @Override
+            protected void populateViewHolder(AcadViewHolder viewHolder, AcadRecords model, int position) {
+                final String acad_key = getRef(position).getKey().toString();
+                viewHolder.setTitle(model.getTitle());
+                viewHolder.setDesc(model.getDesc());
+                viewHolder.setImageUrl(getApplicationContext(),model.getImageUrl());
+                viewHolder.mView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                });
+            }
+        };
+        recyclerView.setAdapter(FBRA);
+    }
+
+    public static class AcadViewHolder extends RecyclerView.ViewHolder {
+        View mView;
+
+        public AcadViewHolder(View itemView) {
+            super(itemView);
+            mView=itemView;
+        }
+
+        public void setTitle(String title) {
+            TextView acad_title = mView.findViewById(R.id.acadTitle);
+            acad_title.setText(title);
+        }
+
+        public void setDesc(String desc) {
+            TextView acad_desc = mView.findViewById(R.id.acadDesc);
+            acad_desc.setText(desc);
+        }
+
+        public void setImageUrl(Context ctx, String imageUrl) {
+            ImageView acad_image = mView.findViewById(R.id.acadImage);
+            Picasso.with(ctx).load(imageUrl).into(acad_image);
+
+        }
+
+
     }
 
 }
