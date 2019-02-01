f99c2223af3b08d32884242652cd0678ddc033b1
jgallos <hsitechnologies@gmail.com>
2019-02-01 11:28:34 +0800

    01/02/2019 11:28am - added attendance

--- app/src/main/java/com/programmer/jgallos/ma_s/AvailClassesActivity.java
+++ app/src/main/java/com/programmer/jgallos/ma_s/AvailClassesActivity.java
@@ -2,20 +2,40 @@ package com.programmer.jgallos.ma_s;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 
 public class AvailClassesActivity extends AppCompatActivity {
 
-    //private Spinner subSpinner;
+    private Button signinBtn;
+
+    private FirebaseDatabase database;
+    private DatabaseReference databaseRef;
+    private DatabaseReference mDatabaseUsers;
+    private FirebaseAuth mAuth;
+    private FirebaseUser mCurrentUser;
+
 
 
     @Override
@@ -44,9 +64,46 @@ public class AvailClassesActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
+        signinBtn = (Button)findViewById(R.id.signinBtn);
+
+        databaseRef = database.getInstance().getReference().child("Android_Development_attendance");
+        mAuth = FirebaseAuth.getInstance();
+        mCurrentUser = mAuth.getCurrentUser();
+        mDatabaseUsers = FirebaseDatabase.getInstance().getReference().child("Users").child(mCurrentUser.getUid());
+
+        signinBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(AvailClassesActivity.this, "Signing-in...", Toast.LENGTH_SHORT).show();
+
+                final DatabaseReference newAttendance = databaseRef.push();
+                mDatabaseUsers.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                        newAttendance.child("uid").setValue(mCurrentUser.getUid()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()){
+                                    startActivity(new Intent(AvailClassesActivity.this,ClassSession.class));
+                                }
+                            }
+                        });
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+            }
+        });
+
     }
 
-    public void onClickSignin(View view) {
+    /* public void onClickSignin(View view) {
 
 
 
@@ -56,5 +113,5 @@ public class AvailClassesActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(), subject, Toast.LENGTH_SHORT).show();
         startActivity(new Intent(AvailClassesActivity.this, ClassSession.class));
         finish();
-    }
+    } */
 }
--- app/src/main/java/com/programmer/jgallos/ma_s/ClassSession.java
+++ app/src/main/java/com/programmer/jgallos/ma_s/ClassSession.java
@@ -14,6 +14,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+
 public class ClassSession extends AppCompatActivity {
 
     @Override
