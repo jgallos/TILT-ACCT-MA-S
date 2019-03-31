fe27102342d92b2720b644522eecc9005c7512ac
jgallos <hsitechnologies@gmail.com>
2019-03-31 23:33:50 +0800

    31/03/2019 11:33pm - feedback replies database nodes

--- app/src/main/java/com/programmer/jgallos/ma_s/FeedbackReplyActivity.java
+++ app/src/main/java/com/programmer/jgallos/ma_s/FeedbackReplyActivity.java
@@ -1,17 +1,36 @@
 package com.programmer.jgallos.ma_s;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class FeedbackReplyActivity extends AppCompatActivity {
 
     String feedback_key = null;
     private TextView feedbackID;
+    private EditText editText_reply;
+    private Button button_send;
+
+    private FirebaseDatabase database;
+    private DatabaseReference databaseRef;
+    private DatabaseReference mDatabaseUsers;
+    private FirebaseAuth mAuth;
+    private FirebaseUser mCurrentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +40,42 @@ public class FeedbackReplyActivity extends AppCompatActivity {
         feedback_key = getIntent().getExtras().getString("SingleFeedbackID");
         feedbackID = (TextView)findViewById(R.id.textViewFID);
 
+        editText_reply = (EditText)findViewById(R.id.editTextReply);
+        button_send = (Button)findViewById(R.id.buttonSend);
+
+        databaseRef = database.getInstance().getReference().child("Reply_" + feedback_key);
+        mAuth = FirebaseAuth.getInstance();
+        mCurrentUser = mAuth.getCurrentUser();
+        mDatabaseUsers = FirebaseDatabase.getInstance().getReference().child("Users").child(mCurrentUser.getUid());
+
         feedbackID.setText(feedback_key);
+
+        button_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String stringReply = editText_reply.getText().toString().trim();
+                final DatabaseReference newReply = databaseRef.push();
+
+                newReply.child("reply").setValue(stringReply);
+                newReply.child("date").setValue("x");
+                newReply.child("time").setValue("x").addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+
+                            //startActivity(new Intent(FeedbackReplyActivity.this, ViewFeedbackActivity.class));
+
+                            Intent reply_back = new Intent(FeedbackReplyActivity.this, SingleFeedbackActivity.class);
+                            reply_back.putExtra("FeedbackID",feedback_key);
+                            startActivity(reply_back);
+                            finish();
+                        }
+                    }
+                });
+
+
+            }
+        });
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
