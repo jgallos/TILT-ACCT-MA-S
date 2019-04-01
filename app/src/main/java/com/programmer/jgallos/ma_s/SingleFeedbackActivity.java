c72e13ee3a6f5ccaafc70efff46d82d7246a5d11
jgallos <hsitechnologies@gmail.com>
2019-04-01 08:09:25 +0800

    1/04/2019 8:09am - feedback replies recyclerview working

--- app/src/main/java/com/programmer/jgallos/ma_s/SingleFeedbackActivity.java
+++ app/src/main/java/com/programmer/jgallos/ma_s/SingleFeedbackActivity.java
@@ -2,16 +2,21 @@ package com.programmer.jgallos.ma_s;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -26,6 +31,13 @@ public class SingleFeedbackActivity extends AppCompatActivity {
     private DatabaseReference mDatabase;
     private Button replyBtn, scalateBtn, resolvedBtn;
     private FirebaseAuth mAuth;
+
+    private RecyclerView recyclerView;
+    private DatabaseReference mReplyDatabase;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseUser mCurrentUser;
+
+    private DatabaseReference mDatabaseUsers;
 
 
     @Override
@@ -48,7 +60,11 @@ public class SingleFeedbackActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         replyBtn.setVisibility(View.INVISIBLE);
 
+        recyclerView = (RecyclerView)findViewById(R.id.recyclerViewSingleFeed);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
 
+        mReplyDatabase = FirebaseDatabase.getInstance().getReference().child("Reply_" + post_key);
 
         mDatabase.child(post_key).addValueEventListener(new ValueEventListener() {
             @Override
@@ -75,6 +91,13 @@ public class SingleFeedbackActivity extends AppCompatActivity {
             }
         });
 
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+            }
+        };
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -92,6 +115,74 @@ public class SingleFeedbackActivity extends AppCompatActivity {
         Intent singleReply = new Intent(this, FeedbackReplyActivity.class);
         singleReply.putExtra("SingleFeedbackID", post_key);
         startActivity(singleReply);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+
+        FirebaseRecyclerAdapter<ReplyRecords, SingleFeedbackActivity.SFBViewHolder> FBRA = new FirebaseRecyclerAdapter<ReplyRecords, SingleFeedbackActivity.SFBViewHolder>(
+                ReplyRecords.class,
+                R.layout.reply_list,
+                SingleFeedbackActivity.SFBViewHolder.class,
+                mReplyDatabase
+        ) {
+            @Override
+            protected void populateViewHolder(SingleFeedbackActivity.SFBViewHolder viewHolder, ReplyRecords model, int position) {
+                mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
+                //mDatabaseUsers = FirebaseDatabase.getInstance().getReference().child("Users").child(mCurrentUser.getUid());
+
+               /* if (!mDatabaseUsers.toString().equals("https://ma-s-514f2.firebaseio.com/Users/" + model.getUid())){
+                    viewHolder.mView.setVisibility(View.GONE);
+                    viewHolder.mView.setLayoutParams(new RecyclerView.LayoutParams(0,0));
+                } else {
+                    viewHolder.mView.setVisibility(View.VISIBLE);
+                } */
+
+                final String reply_key = getRef(position).getKey().toString();
+
+                viewHolder.setDate(model.getDate());
+                viewHolder.setTime(model.getTime());
+                viewHolder.setReply(model.getReply());
+
+
+                //Toast.makeText(ViewAttendanceActivity.this, model.getSignout().toString(), Toast.LENGTH_LONG).show();
+                viewHolder.mView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                });
+            }
+        };
+        recyclerView.setAdapter(FBRA);
+    }
+
+
+    public static class SFBViewHolder extends RecyclerView.ViewHolder {
+        View mView;
+
+        public SFBViewHolder(View itemView) {
+            super(itemView);
+            mView=itemView;
+        }
+
+        public void setDate(String date) {
+            TextView response_date = mView.findViewById(R.id.textViewResponseDate);
+            response_date.setText("Date: " + date);
+        }
+
+        public void setTime(String time) {
+            TextView response_time = mView.findViewById(R.id.textViewResponseTime);
+            response_time.setText("Sign-in Time: " + time);
+        }
+
+        public void setReply(String reply) {
+            TextView response_reply = mView.findViewById(R.id.textViewResponse);
+            response_reply.setText("Reply: " + reply);
+        }
+
     }
 
 }
