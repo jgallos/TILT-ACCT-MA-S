5f35e46ad75ae63424ad8fa7d078691809d8f040
jgallos <hsitechnologies@gmail.com>
2019-05-08 18:50:55 +0800

    08/05/2019 6:50pm - notification system connected and working

--- app/src/main/java/com/programmer/jgallos/ma_s/NotificationActivity.java
+++ app/src/main/java/com/programmer/jgallos/ma_s/NotificationActivity.java
@@ -1,25 +1,36 @@
 package com.programmer.jgallos.ma_s;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class NotificationActivity extends AppCompatActivity {
@@ -30,6 +41,8 @@ public class NotificationActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private FirebaseUser mCurrentUser;
     private DatabaseReference mNotifDatabase;
+
+    Boolean firstFlag = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,7 +66,60 @@ public class NotificationActivity extends AppCompatActivity {
             }
         };
 
+        mNotifDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //Toast.makeText(NotificationActivity.this,"sdfsd",Toast.LENGTH_SHORT).show();
+                if (firstFlag == true) {
+                    firstFlag = false;
+                } else {
 
+                    String notifText = (String) dataSnapshot.child("nnotif").getValue();
+                    String notifDate = (String) dataSnapshot.child("ndate").getValue();
+                    String notifTime = (String) dataSnapshot.child("ntime").getValue();
+
+                    NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                    String NOTIFICATION_CHANNEL_ID = "my_channel_id_01";
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "My Notifications", NotificationManager.IMPORTANCE_HIGH);
+
+                        // Configure the notification channel.
+                        notificationChannel.setDescription("Channel description");
+                        notificationChannel.enableLights(true);
+                        notificationChannel.setLightColor(Color.RED);
+                        notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+                        notificationChannel.enableVibration(true);
+                        notificationManager.createNotificationChannel(notificationChannel);
+                    }
+
+
+                    NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(NotificationActivity.this, NOTIFICATION_CHANNEL_ID);
+
+                    Intent intent = new Intent(NotificationActivity.this, NotificationActivity.class);
+                    PendingIntent pendingIntent = PendingIntent.getActivity(NotificationActivity.this, 0, intent, 0);
+
+                    notificationBuilder.setAutoCancel(true)
+                            .setDefaults(Notification.DEFAULT_ALL)
+                            .setWhen(System.currentTimeMillis())
+                            .setSmallIcon(R.drawable.login)
+                            .setContentIntent(pendingIntent)
+                            .setTicker("Program designed by: J.Gallos")
+                            // .setPriority(Notification.IMPORTANCE_HIGH)
+                            .setContentTitle("TILT-ACCT System")
+                            .setContentText("New notification received.")
+                            .setContentInfo("Info");
+
+                    notificationManager.notify(/*notification id*/1, notificationBuilder.build());
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
 
 
